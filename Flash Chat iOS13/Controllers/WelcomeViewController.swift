f6a55654a796f0e346b041711ea52abde70dcc47
nastyabeggin <45201186+nastyabeggin@users.noreply.github.com>
2023-01-03 23:28:07 +0300

    Add animation

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
@@ -14,7 +15,18 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let textForLabel = "⚡️FlashChat"
+        
+        for letter in textForLabel{
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex,
+                                 repeats: false){ ( timer ) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
        
     }
     
