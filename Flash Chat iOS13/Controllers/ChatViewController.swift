a5433e68b40ef49674192d1fb2b71c45560c8748
nastyabeggin <45201186+nastyabeggin@users.noreply.github.com>
2023-01-17 18:12:22 +0300

    Create loadMessages

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,21 +14,62 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
-    var messages: [Message] = [
-        Message(sender: "123@gmail.com", body: "Hi!"),
-        Message(sender: "nastyabeginina@gmail.com", body: "Hello!"),
-        Message(sender: "123@gmail.com", body: "What's up?"),
-
-    ]
+    let db = Firestore.firestore()
+    
+    var messages: [Message] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
         navigationItem.hidesBackButton = true
+        
+        loadMessages()
+    }
+    
+    func loadMessages(){
+        
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { (querySnapshot, error) in
+            self.messages = []
+            if let e = error {
+                print("Issue retrieving data from database. \(e)")
+            } else {
+                if let snapshotDocuments = querySnapshot?.documents {
+                    for doc in snapshotDocuments {
+                        let data = doc.data()
+                        if let sender = data[K.FStore.senderField] as? String,
+                           let body = data[K.FStore.bodyField] as? String {
+                            let newMessage = Message(sender: sender,
+                                                     body: body)
+                            self.messages.append(newMessage)
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection(K.FStore.collectionName).addDocument(data:[
+                K.FStore.senderField: messageSender,
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField: Date().timeIntervalSince1970
+            ]) { (error) in
+                if let e = error{
+                    print("Issue adding data to database. \(e)")
+                } else {
+                    print("Successfully saved data")
+                }
+            }
+        }
+        
     }
     
 
--- Podfile
+++ Podfile
@@ -1,6 +1,11 @@
 platform :ios, '13.0'
+use_frameworks! :linkage => :static
 
 target 'Flash Chat iOS13' do
+  pod 'Firebase'
+  pod 'FirebaseFirestore'
+  pod 'FirebaseAuth'
+  pod 'IQKeyboardManager', '~> 6.5'
 
   # Pods for Flash Chat iOS13
   
